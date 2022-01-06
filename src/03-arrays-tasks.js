0a6c89aafd850d1f41f0f0b1abdb3a002a453676
Alex <olivesargreek@gmail.com>
2022-01-06 12:51:19 +0300

    fix: task insertItem into 03-aarays-tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -147,7 +147,8 @@ function getStringsLength(/* arr */) {
  *    [ 1, 'b', 'c'], 0, 'x'  => [ 'x', 1, 'b', 'c' ]
  */
 function insertItem(arr, item, index) {
-  return arr.splice(index, 0, item);
+  arr.splice(index, 0, item);
+  return arr;
 }
 
 /**
